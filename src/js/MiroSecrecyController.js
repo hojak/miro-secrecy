6ada8061919bf37f898cf422940086e89e91474e
Holger Janßen-Kroll <holger.janssen-kroll@aleri.de>
2021-07-06 02:50:34 +0200

    'encryption' of cards to base64 in description working

--- src/js/MiroSecrecyController.js
+++ src/js/MiroSecrecyController.js
@@ -1,7 +1,10 @@
 const iconLock = '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"/>'
-const iconUnlock = '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="1"/>'
 
 const tagName = 'Top Secret'
+
+const secretPassword = "isefhiuqwefiuabwfu"
+
+const APP_ID = '3074457361019166452'
 
 class MiroSecrecyController {
   constructor (miro) {
@@ -10,24 +13,9 @@ class MiroSecrecyController {
 
     $this.miro.onReady(() => {
       $this.addLockButton()
-      // $this.addUnlockButton()
     })
   }
 
-  addUnlockButton () {
-    console.log('adding unlock button!')
-    const $this = this
-
-    $this.miro.initialize({
-      extensionPoints: {
-        bottomBar: {
-          title: 'Decrypt Top Secret Widgets',
-          svgIcon: iconUnlock,
-          onClick: () => $this.handleUnlockButtonClick()
-        }
-      }
-    })
-  }
 
   addLockButton () {
     console.log('adding lock button')
@@ -62,29 +50,87 @@ class MiroSecrecyController {
     const $this = this
     await $this.checkAuthorization()
 
-    console.log('lock!')
-
-    $this.encryptTaggedWidgets()
+    $this.hasEncryptedWidgets().then ( answer => {
+        if ( answer )  {
+            $this.decryptTaggedWidgets()
+        } else {
+            $this.encryptTaggedWidgets()
+        }
+    })
   }
 
-  async handleUnlockButtonClick () {
-    const $this = this
-    await $this.checkAuthorization()
-
-    console.log('unlock!')
-  }
 
   async encryptTaggedWidgets () {
-    await this.miro.board.widgets.get().then(
-      data => data.filter(
-        item => (item.tags.map(tag => tag.title).filter(name => name === tagName).length) > 0
-      )
-        .forEach(element => {
-          console.log('found item:')
-          console.log(element)
+    console.log ( 'encrypt' )
+    const $this = this
+    await $this.miro.board.widgets.get({type: 'CARD'}).then(
+      data => {
+          
+        let updateData = data.filter(
+            item => (item.tags.map(tag => tag.title).filter(name => name === tagName).length) > 0
+        ).map(element => {
+            let data = {
+                version: '0.1',
+                title: window.btoa( element.title),
+                description: window.btoa( element.description)
+            }
+
+            return {
+                id: element.id,
+                title: 'TOP SECRET',
+                description: 'ENCRYPTED...' + JSON.stringify(data)
+            }
         })
+        
+        $this.miro.board.widgets.update ( updateData )
+    })
+  }
+
+  async decryptTaggedWidgets () {
+    console.log ( 'decrypt' )
+    const $this = this
+    await $this.miro.board.widgets.get({type: 'CARD'}).then(
+      data => {
+          
+        let updateData = data.filter(
+            item => item.description.startsWith ( 'ENCRYPTED...')
+        ).map(element => {
+            let parsed = element.description
+            .substring ( 'ENCRYPTED...'.length)
+            .replaceAll ('<br>','')
+            .trim() 
+            .replaceAll ('&#34;', '"')
+            .replaceAll ( '&#43;', '+')
+            .replaceAll ( '&#61;', '=')
+
+            console.log ( parsed )
+
+            let data = JSON.parse ( parsed )
+
+            return {
+                id: element.id,
+                title: window.atob(data.title),
+                description: window.atob(data.description)
+            }
+        })
+        
+        $this.miro.board.widgets.update ( updateData )
+    })
+  }
+
+  async hasEncryptedWidgets () {
+    return await this.miro.board.widgets.get({type: 'CARD'}).then ( 
+        data => {
+            return data.filter ( 
+                item => item.description.startsWith ( 'ENCRYPTED...')
+            ).length > 0
+        }
     )
   }
+
+
+
+
 }
 
 module.exports = MiroSecrecyController
